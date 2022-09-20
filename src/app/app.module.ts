cd298d9025740f9c8f0f7549bfb87b51781917f5
akyarbrough <ayarbrough0816@gmail.com>
2022-09-19 20:30:16 -0500

    Exercise 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParagraphListComponent } from './paragraph-list/paragraph-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParagraphListComponent
   ],
   imports: [
     BrowserModule,
